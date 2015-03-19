73a298c365a129502d01523ab96a2c7c50b915e4
j540 <james@tokyowheel.com>
2015-03-19 16:41:27 +0700

    update deface for spree 3-0-stable

--- app/overrides/add_currency_rate_to_admin_configuration_sidebar.rb
+++
@@ -1,8 +0,0 @@
-
-
-Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_configuration",
-                     :name => "currency_rate_admin_menu",
-                     :insert_bottom => '#sidebar-product',
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::CurrencyRate) %><%= configurations_sidebar_menu_item Spree.t(:currency_rates), spree.admin_currency_rates_path %><% end %>",
-                     :disabled => false)
-
